d3cc6154e971dc492fcc19bc304e9796fb4dc8e6
one-samar <sosama@onesolutionc.com>
2022-10-29 16:31:17 +0200

    [Add] updateApp.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
 
       <Provider store={questionStore}  >
- <BrowserRouter baseurl={process.env.PUBLIC_URL}>
+ <BrowserRouter>
  <Routes/>
  </BrowserRouter>
     
